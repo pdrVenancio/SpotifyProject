59c7d09a1f194b03b12da0b84ea7f15e88cffce3
pdrVenancio <pedrosantos121229@gmail.com>
2024-01-25 18:40:04 -0300

    Pesquisa corrigida

---
+++ script.js
@@ -0,0 +1,41 @@
+const searchInput = document.getElementById('search-input');
+const resultArtist = document.getElementById("result-artist");
+const resultPlaylist = document.getElementById('result-playlists');
+
+function requestApi(searchTerm) {
+
+    // Query marametro
+    // Vai na api e pega name_like = nome como o nome de artista buscado
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+    // fech - Usado para fazer requisiçoes de api
+    fetch(url)
+        .then((response) => response.json())
+        .then((result) => displayResults(result))
+}
+
+function displayResults(result) {
+    resultPlaylist.classList.add("hidden")
+    const artistName = document.getElementById('artist-name');
+    const artistImage = document.getElementById('artist-img');
+
+    result.forEach(element => {
+        artistName.innerText = element.name;
+        artistImage.src = element.urlImg;
+    });
+
+    resultArtist.classList.remove('hidden');
+}
+
+// Quando o evento imput acontecer ...
+document.addEventListener('input', function () {
+   
+    const searchTerm = searchInput.value.toLowerCase();
+    if (searchTerm === '') {
+        resultPlaylist.classList.add('hidden');
+        resultArtist.classList.remove('hidden');
+        return
+    }
+    
+    requestApi(searchTerm);
+})
+ 
